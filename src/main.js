fee51ef53fea5e9e974b4ea678546bca78f1247c
Svea Wade <svealinneawade@gmail.com>
2020-11-20 15:30:59 -0800

    added some styling and verbiage to the responses

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import './css/styles.css';
 import CurrencyAPI from './currency.js';
 
 $(document).ready(function() {
-  $('form#currency').submit(function() {
+  $('form#currency1').submit(function() {
     event.preventDefault();
     let dollars = $('#input').val();
     $('#input').val("");
@@ -15,17 +15,17 @@ $(document).ready(function() {
       let value = $("select#currency").val();
       console.log(value);
         if (value == 1) {
-          $("#spain").append(`Your conversion is € ${main.conversion_rates.EUR * dollars}`);
+          $("#spain").append(`Your conversion to the Euro is € ${main.conversion_rates.EUR * dollars}`);
         } else if (value == 2) {
-          $('#australia').append(`Your conversion is $ ${main.conversion_rates.AUD * dollars}`);
+          $('#australia').append(`Your conversion to the Australian dollar is $ ${main.conversion_rates.AUD * dollars}`);
         } else if (value == 3) {
-          $("#china").append(`Your conversion is ¥ ${main.conversion_rates.CNY * dollars}`);
+          $("#china").append(`Your conversion to the Chinese yuan is ¥ ${main.conversion_rates.CNY * dollars}`);
         } else if (value == 4) {
-          $("#uk").append(`Your conversion is £ ${main.conversion_rates.GBP * dollars}`);
+          $("#uk").append(`Your conversion to the English pound is £ ${main.conversion_rates.GBP * dollars}`);
         } else if (value == 5) {
-          $("#nz").append(`Your conversion is $ ${main.conversion_rates.NZD * dollars}`)
+          $("#nz").append(`Your conversion to the New Zealand dollar is $ ${main.conversion_rates.NZD * dollars}`)
         } else if (value == 6) {
-          $("#mexico").append(`Your conversion is $ ${main.conversion_rates.MXN * dollars}`)
+          $("#mexico").append(`Your conversion to the Mexican peso is $ ${main.conversion_rates.MXN * dollars}`)
         } else {
           $("#error").show();
         }
