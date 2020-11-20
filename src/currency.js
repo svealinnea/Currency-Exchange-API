9f9212b4127e0ae242a8f54dda6e7ce63482ad2c
Svea Wade <svealinneawade@gmail.com>
2020-11-20 09:50:20 -0800

    getting CORS error, troubleshooting that

--- src/currency.js
+++ src/currency.js
@@ -1,3 +1,22 @@
+export default class CurrencyAPI {
+  static getExchange() { 
+      return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/${process.env.API_KEY}/latest/USD`;
+      
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        }
+        else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send(); 
+    });
+  }
+}
 
 
 
