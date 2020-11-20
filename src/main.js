c3214e5b1cf2de1f2520f87b73006c8f5343b3d3
Svea Wade <svealinneawade@gmail.com>
2020-11-20 11:59:48 -0800

    Spain value appends to the page when it is selected

--- src/main.js
+++ src/main.js
@@ -4,24 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyAPI from './currency.js';
 
-  $('#currency').click(function() {
+  $('form#currency').submit(function() {
     event.preventDefault();
     let dollars = $('#input').val();
-    $('#input').val("");
-    console.log(dollars)
+    //$('#input').val("");
     let promise = CurrencyAPI.getExchange(dollars);
     promise.then(function(response) {
       const main = JSON.parse(response);
-      console.log(main);
-      let spain = main.conversion_rates.EUR * dollars
-      let australia = main.conversion_rates.AUD * dollars
-      let china = main.conversion_rates.CNY * dollars
-      let england = main.conversion_rates.GBP * dollars
-      let nz = main.conversion_rates.NZD * dollars
-      console.log(nz)
-      console.log(england);
-      console.log(china);
-      console.log(australia);
-      console.log(spain);
-    })
+      let value = $("select#currency").val();
+      console.log(value);
+      let spain = main.conversion_rates.EUR * dollars;
+      // let australia = main.conversion_rates.AUD * dollars;
+      // let china = main.conversion_rates.CNY * dollars;
+      // let england = main.conversion_rates.GBP * dollars;
+      // let nz = main.conversion_rates.NZD * dollars;
+      // let mexico = main.conversion_rates.MXN * dollars;
+        if (value == 1) {
+        $("#spain").append("Your conversion is " + "€" + spain)
+        }
+      });
   });
