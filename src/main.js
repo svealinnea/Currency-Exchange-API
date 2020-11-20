8e51b531420db588fc7cc954459ff00950f9d669
Svea Wade <svealinneawade@gmail.com>
2020-11-20 10:25:06 -0800

    spain is working for pulling the amount. .ENV file is not working so I will not be uploading the currency.js until I figure that out.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import {} from './currency.js';
+import CurrencyAPI from './currency.js';
+
+  $('#currency').click(function() {
+    event.preventDefault();
+    let dollars = $('#input').val();
+    $('#input').val("");
+    console.log(dollars)
+    let promise = CurrencyAPI.getExchange(dollars);
+    promise.then(function(response) {
+      const main = JSON.parse(response);
+      console.log(main);
+      let spain = main.conversion_rates.EUR * dollars
+      console.log(spain);
+    })
+  });
